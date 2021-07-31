6c209da2dcca83c5232f29ab254367ba547f9dbb
AbijuruT <abijuruyves@gmail.com>
2021-07-31 05:01:18 +0200

    My day1 project

---
+++ Day1.py
@@ -0,0 +1,9 @@
+# This program can generate Nicknames according to user inputs
+
+print("Welcome to Nickname generator!")
+# Get user inputs
+user_lastname = input("What is your last name?\n")
+favorite_person = input("Who do you like the most?\n")
+# Now Concatinate those inputs to get one name
+print("Your nickname could be "+user_lastname+" "+favorite_person)
+print("Or "+favorite_person+" "+user_lastname)

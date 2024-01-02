dedad1510b5a6d81980e700dfdd6a0bd149c9e1a
Thor <abijuruyves@gmail.com>
2024-01-02 13:14:59 +0200

    Removed a comment

--- Day1.py
+++ Day1.py
@@ -4,6 +4,5 @@ print("Welcome to Nickname generator!")
 # Get user inputs
 user_lastname = input("What is your last name?\n")
 favorite_person = input("Who do you like the most?\n")
-# Now Concatinate those inputs to get one name
 print("Your nickname could be " + user_lastname + " " + favorite_person)
 print("Or " + favorite_person + " " + user_lastname)
